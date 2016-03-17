a83797f9f5afdad0962dbe4ff251b069104b8c34
horic <mail@minfun.net>
2016-03-17 12:18:20 +0000

    log_cutting bash code

--- log_cutting.sh
+++ log_cutting.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+LOGPATH="/var/log/access.log"
+SAVEPATH="/var/log/"
+PIDPATH="/var/run/nginx.pid"
 
+mv $LOGPATH $SAVEPATH`date '+%Y-%m-%d' --date="-1 day"`.log
+touch $LOGPATH
+
+kill -HUP `cat $PIDPATH`
