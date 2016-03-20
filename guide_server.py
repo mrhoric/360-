b8314a1773571972d2080859a2916b6313882192
horic <mail@minfun.net>
2016-03-20 07:22:37 +0000

    修改异常处理，增加稳定性；增加信号量处理

--- guide_server.py
+++ guide_server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-import socket,threading
+import socket,threading,signal,errno
 
 host = socket.gethostname()
 port = 4000
@@ -7,16 +7,26 @@ maxclient = 40
 bufsize = 1024
 ERRORPATH = '/var/log/guide_server_error.log'
 VALUEPATH = '/var/guide_value'
+cdic = {}
+
+def SigDeal(a,b):
+    global cdic
+    cdic = ReadValue() 
 
 def ReadValue():
     cdic = {}
-    f = open(VALUEPATH,'r')
-    for i in f.readlines():
-        tmp = i.split('=')
-        cdic[tmp[0]] = tmp[1].replace('\n','')
+    try:
+        f = open(VALUEPATH,'r')
+        for i in f.readlines():
+            tmp = i.split('=')
+            cdic[tmp[0]] = tmp[1].replace('\n','')
+    except IOError,e:
+        f = open(ERRORPATH,'a+')
+        f.writelines('ValueFile:'+str(e)+'\n')
     return cdic
 
-def NetDeal(c,cdic):
+def NetDeal(c):
+    global cdic
     while 1:
         data = c.recv(bufsize)
         if not data:
@@ -28,18 +38,20 @@ def NetDeal(c,cdic):
     c.close()
 
 if __name__ == '__main__':
-    try:
-        cdic = ReadValue()
-        s = socket.socket()
-        s.bind((host,port))
-        s.listen(maxclient)
-        while 1:
+    cdic = ReadValue()
+    s = socket.socket()
+    s.bind((host,port))
+    s.listen(maxclient)
+    signal.signal(signal.SIGUSR1,SigDeal)
+    while 1:
+        try:
             c,addr = s.accept()
             if c:
-                t = threading.Thread(target=NetDeal,args=(c,cdic))
+                t = threading.Thread(target=NetDeal,args=(c,))
                 t.start()
-
-    except Exception,e:
-        f = open(ERRORPATH,'a+')
-        print e 
-        f.writelines(str(e) + '\n')
+        except Exception,e:
+            if e.errno == errno.EINTR:
+                continue
+            f = open(ERRORPATH,'a+')
+            print e 
+            f.writelines(str(e) + '\n')
